029b47e0077318bd0656676f65f427703ce745c0
neilwoodroffe <neilowoodroffe@hotmail.com>
2019-06-27 20:24:48 +1000

    updated margin

--- sketch.js
+++ sketch.js
@@ -93,7 +93,7 @@ image(img, b, a,);
 
 //textSize(vol*windowWidth/5);
 textSize(12 + (mouseX / width)*72);
-text('Hello World', 100, 90);
+text('Hello World', 220, 90);
 fill(0, 102, 153);
 image(pimg, mouseX, mouseY, pimg.width * 1.5, pimg.height * 1.5);
 
